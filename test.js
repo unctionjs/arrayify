abbacf3e7cde47fbe5e2ea58422aeb9679c15cff
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:11:17 -0700

    Woops, totally fucked up these paths

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import applicators from "./source"
+import arrayify from "./source"
 
 test(({same, end}) => {
   same(
