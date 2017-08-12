b3ddae0a73ca40a4d40350359c4985c333ec5cfb
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:06:18 -0700

    Don't bother using the extension, eslint complains

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import arrayify from "./source.js"
+import applicators from "./source"
 
 test(({same, end}) => {
   same(
